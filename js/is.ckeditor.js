6684c71150ee077da7e1ef7f1552abc48ab746e6
sudwebdesign <thomas@sudwebdesign.fr>
2015-02-15 07:08:30 +0100

    clean

--- js/is.ckeditor.js
+++ js/is.ckeditor.js
@@ -5,13 +5,7 @@ $js([
 ],function(){
 	$('[is=ckeditor]').each(function(){
 		$(this).wrap('<div/>');
-		
-		CKEDITOR.basePath = $('base').attr('href')+'js/plugin/ckeditor/';//moved in js/ 4 resolve 1 incompréhensive bug
-		alert('cqcb: '+CKEDITOR.basePath);
-//		CKEDITOR.basePath = CKEDITOR.basePath.replace(/\/js/i, '');
-		if(console) console.info(CKEDITOR.basePath);
+		CKEDITOR.basePath = $('base').attr('href')+'js/plugin/ckeditor/';
 		CKEDITOR.replace(this);
 	});
 });
-//http://localhost/Autonomous/Surikat/js/plugin/ckeditor/ckeditor.js ok
-//http://localhost/Autonomous/js/plugin/ckeditor/ckeditor.js  mirroring ok
