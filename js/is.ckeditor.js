e5263eb95e18a81c6aac105b2f6248f91d816324
sudwebdesign <thomas@sudwebdesign.fr>
2015-02-15 07:05:05 +0100

    fix is.ckeditor path (maybe highlight)
    ckeditor && highlightmoved folders to js/

--- js/is.ckeditor.js
+++ js/is.ckeditor.js
@@ -1,11 +1,17 @@
-<!--#include virtual="../../plugin/ckeditor/ckeditor.js" -->
+<!--#include virtual="../plugin/ckeditor/ckeditor.js" -->
 $js([
 	'jquery',
-	//'/plugin/ckeditor/ckeditor.js'
+	'plugin/ckeditor/ckeditor.js'
 ],function(){
 	$('[is=ckeditor]').each(function(){
 		$(this).wrap('<div/>');
-		CKEDITOR.basePath = $('base').attr('href')+'plugin/ckeditor/';
+		
+		CKEDITOR.basePath = $('base').attr('href')+'js/plugin/ckeditor/';//moved in js/ 4 resolve 1 incompréhensive bug
+		alert('cqcb: '+CKEDITOR.basePath);
+//		CKEDITOR.basePath = CKEDITOR.basePath.replace(/\/js/i, '');
+		if(console) console.info(CKEDITOR.basePath);
 		CKEDITOR.replace(this);
 	});
 });
+//http://localhost/Autonomous/Surikat/js/plugin/ckeditor/ckeditor.js ok
+//http://localhost/Autonomous/js/plugin/ckeditor/ckeditor.js  mirroring ok
